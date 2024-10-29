0e0978abc1b38c4151e566ebd7c1adf6a2fe4672
tyagnii <alelsandr@tyagnii.ru>
2024-10-29 15:00:03 +0300

    models

---
+++ internal/handlers/models.go
@@ -0,0 +1,22 @@
+package handlers
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type Currency struct {
+	USD float32 `json:"USD"`
+	RUB float32 `json:"RUB"`
+	EUR float32 `json:"EUR"`
+}
+
+type Wallet struct {
+	Message string   `jsong:"message"`
+	Balance Currency `json:"balance"`
+}
+
+type Rates struct {
+	Rates Currency `jsong:"rates"`
+}
