d47be7435b75048ff0568df3e8dd8380ffeb28a9
tyagnii <aleksandr.tyagni@gmail.com>
2024-11-02 20:11:33 +0300

    Modify token pkg

--- pkg/token/token.go
+++ pkg/token/token.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/tyagnii/gw-currency-wallet/internal/models"
 	"time"
@@ -8,6 +9,11 @@ import (
 
 var Secret string
 var ExpireTime time.Duration
+
+type Claims struct {
+	jwt.RegisteredClaims
+	Username string
+}
 
 // Init package variables
 func init() {
@@ -20,12 +26,12 @@ func init() {
 
 // NewToken creates new JWT token for given user
 func NewToken(user models.User) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-
-	claims := token.Claims.(jwt.MapClaims)
-	claims["username"] = user.Username
-	claims["email"] = user.Email
-	claims["exp"] = time.Now().Add(ExpireTime).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(ExpireTime)),
+		},
+		Username: user.Username,
+	})
 
 	tokenString, err := token.SignedString([]byte(Secret))
 	if err != nil {
@@ -33,4 +39,30 @@ func NewToken(user models.User) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+// ParseToken checks if provided token is valid and
+// return error if its not
+// or token structure if it is valid
+func ParseToken(tokenString string) (bool, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+		return []byte(Secret), nil
+	})
+
+	// TODO: handle error separately from token checks
+	if err != nil {
+		return false, err
+	}
+
+	if !token.Valid {
+		return false, fmt.Errorf("invalid token")
+	}
+
+	return true, nil
+}
+
+func GetUsernameFromClaims(token *jwt.Token) (string, error) {
+	return token.Claims.(jwt.MapClaims)["username"].(string), nil
 }
