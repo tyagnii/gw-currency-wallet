27a96b52ce4f58482e59ac3c154bdfd50bea770f
tyagnii <aleksandr.tyagni@gmail.com>
2024-11-06 13:11:38 +0300

    Tricky trick for pull request

--- internal/db/errors.go
+++
@@ -1,7 +0,0 @@
-package db
-
-import "errors"
-
-var ErrorUserNotFound = errors.New("user not found")
-var ErrorInsufficientBalance = errors.New("insufficient balance")
-var ErrorAuthenticationFailed = errors.New("authentication failed")
